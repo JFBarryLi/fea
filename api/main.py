9531a4a1b5feb8e0e5083390bd68832ed9ec9df4
Barry Li <bli031@uottawa.ca>
2020-12-22 04:32:27 +0000

    Added CORS.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import truss
 
@@ -6,6 +7,22 @@ fea_app = FastAPI(
     title='fea-app api',
     description='Api for finite element solvers.',
     version='v1'
+)
+
+origins = [
+    'http://localhost',
+    'http://localhost:3000',
+    'http://192.168.0.102',
+    'http://192.168.0.102:3000',
+    'https://barryli.ca',
+    'https://fea.barryli.ca',
+]
+
+fea_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['GET', 'POST'],
+    allow_headers=['*'],
 )
 
 
