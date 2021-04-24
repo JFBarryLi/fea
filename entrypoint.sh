fe1f93b36488abda58df6cdbaa4c0d5eb9825584
Barry Li <bli031@uottawa.ca>
2021-04-24 21:48:10 +0000

    Updated container port.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /fea-app
-gunicorn -w 4 --forwarded-allow-ips="*" -k uvicorn.workers.UvicornH11Worker api.main:fea_app --bind 0.0.0.0:5900
+gunicorn -w 4 --forwarded-allow-ips="*" -k uvicorn.workers.UvicornH11Worker api.main:fea_app --bind 0.0.0.0:80
